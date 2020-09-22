f9e29e09883d0bd0369a93d80a0a3b0504fe0357
Ben Freudman <bfreud94@gmail.com>
2020-09-22 12:37:13 -0400

    Moved CSVs to Google Cloud; extremely slow API call (30 seconds)

--- src/api/housingData.js
+++ src/api/housingData.js
@@ -1,17 +1,25 @@
 // Imports for external dependencies
 const express = require('express');
 const csv = require('async-csv');
-const fs = require('fs').promises;
 const zipcodes = require('zipcodes');
-const path = require('path');
 const router = express.Router();
 const { options } = require('../util/dropdownOptions');
+const { Storage } = require('@google-cloud/storage');
 
 router.get('/data', async (req, res, next) => {
     try {
-        const filePath = path.join(__dirname, `../resources/Homes/${options(req.query['mapType'])}`);
-        const csvString = await fs.readFile(filePath, 'utf-8');
-        const rows = await csv.parse(csvString);
+        const gc = new Storage({
+            credentials: {
+                client_email: process.env.client_email,
+                private_key: process.env.private_key.replace(/\\n/g, '\n')
+            },
+            projectId: process.env.projectId
+        });
+        let contents = await new Promise((resolve, reject) => {
+            let contents = '';
+            gc.bucket('csv-data').file(`${options(req.query['mapType'])}`).createReadStream().on('data', (d) => contents += d).on('end', () => resolve(contents));
+        });
+        const rows = await csv.parse(contents);
         const data = [];
         rows.forEach((row, index) => {
             if(index !== 0) {
